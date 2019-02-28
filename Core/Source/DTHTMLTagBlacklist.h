993c2626d9d061d342279d019a4453a6358575da
Ostap Holub <holub.ostap@gmail.com>
2019-02-28 13:47:04 +0200

    add blacklist class for tags to ignore inline attributes

---
+++ Core/Source/DTHTMLTagBlacklist.h
@@ -0,0 +1,17 @@
+//
+//  DTHTMLTagBlacklist.h
+//  DemoApp
+//
+//  Created by Ostap Holub on 2/28/19.
+//  Copyright © 2019 Drobnik.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTHTMLTagBlacklist : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Core/Source/DTHTMLTagBlacklist.m
@@ -0,0 +1,13 @@
+//
+//  DTHTMLTagBlacklist.m
+//  DemoApp
+//
+//  Created by Ostap Holub on 2/28/19.
+//  Copyright © 2019 Drobnik.com. All rights reserved.
+//
+
+#import "DTHTMLTagBlacklist.h"
+
+@implementation DTHTMLTagBlacklist
+
+@end
