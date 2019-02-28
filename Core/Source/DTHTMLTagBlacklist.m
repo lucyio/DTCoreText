85916c1dd88fd5dc22123e8ef9432aa389295669
Ostap Holub <holub.ostap@gmail.com>
2019-02-28 13:58:41 +0200

    add init method for singleton

--- Core/Source/DTHTMLTagBlacklist.m
+++ Core/Source/DTHTMLTagBlacklist.m
@@ -10,4 +10,14 @@
 
 @implementation DTHTMLTagBlacklist
 
++ (instancetype)sharedInstance {
+	
+	static DTHTMLTagBlacklist *sharedBlacklist = nil;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		sharedBlacklist = [[self alloc] init];
+	});
+	return sharedBlacklist;
+}
+
 @end
